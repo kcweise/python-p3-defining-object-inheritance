3f5f229a06adb20edd9d171e2762fd3e2a26b513
Keenan Weise <kcweise@gmail.com>
2024-04-06 08:54:09 -0500

    Done

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,14 @@
 class Vehicle:
+    
+    def __init__(self, wheel_size, wheel_number, color="red"):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+        self.color = color
+
+    def go(self):
+        return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+        return "filling up!"
+
     pass
